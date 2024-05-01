c34859a75fb1c04a613654918f712db1a97cc8c9
--__-- <karlen.h.1995@gmail.com>
2024-05-01 14:06:02 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Պատրաստ ֆունկցիաներ
+//left()
+//color()
+//width()
+//forward()
+
+
+// color ("aqua")
+// width (10)
+// for(let i = 0 ; i < 6; i++){
+//         forward (150)
+//         left (60)
+// }
+
+
